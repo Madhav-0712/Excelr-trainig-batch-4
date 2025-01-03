0849d3fcea686951bdaf2b5aa6fe897d13b1dc5b
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-03 10:12:57 +0530

    Verifying of username and password 2

---
+++ demo006.java
@@ -0,0 +1,10 @@
+public class demo006 {
+    public static void main(String[] args) {
+        String username = "Madhav_006";
+        String password = "Madhav@006";
+        while ((username == "Madhav_006") && (password == "Madhav@006")) {
+            System.out.println("Welcome to 2025");
+            break;
+        }
+    }
+}
