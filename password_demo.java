790c27cb37a6bf16b074d00f5b73a357de9aa5c7
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-03 10:01:35 +0530

    Verifying Username and Password

---
+++ password_demo.java
@@ -0,0 +1,12 @@
+public class password_demo {
+
+    public static void main(String[]args){
+        String username = "user001"; 
+        String pwd ="user_001";
+        while((username == "user001") && (pwd == "user_001")){
+            System.out.println("username and password are correct");
+            break;
+        }
+    }
+
+}
