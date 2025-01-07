798c0fb2d8a0b8b454268427ec64005bc5b5aed3
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-07 10:19:37 +0530

    Unary operators

---
+++ demo13.java
@@ -0,0 +1,9 @@
+public class demo13{
+    public static void main(String[]args){
+        int a = 10;
+        a++;
+        a--;
+        System.out.println("increment of a is: "+a++);
+        System.out.println("Decrement of a is: "+a--); 
+    }
+}
