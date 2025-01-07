ca476803c45fd85b67cdc5eed61da0960ba427bb
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-07 10:23:57 +0530

    Unary Operators

--- demo13.java
+++ demo13.java
@@ -1,9 +1,14 @@
 public class demo13{
     public static void main(String[]args){
         int a = 10;
-        a++;
-        a--;
-        System.out.println("increment of a is: "+a++);
-        System.out.println("Decrement of a is: "+a--); 
+    
+        System.out.println("Postincrement of a is: "+a++);
+        System.out.println("Postdecrement of a is: "+a--); 
+        System.out.println("Predecrement of a is: "+--a);
+        System.out.println("Preincrement of a is: "+ ++a); 
+ 
+
+
+        
     }
 }
