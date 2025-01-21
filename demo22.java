03273df70acd051eef9c4e7634da5c0108201d6d
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-21 10:10:00 +0530

    printing numbers from 1 to 10 except 5 and 7

---
+++ demo22.java
@@ -0,0 +1,12 @@
+public class demo22 {
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i=0;i<=10;i++){
+            if(i ==5|| i==7){
+                continue;
+            }
+            System.out.print(i);
+
+        }
+    }
+}
