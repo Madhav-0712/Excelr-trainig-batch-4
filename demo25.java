d7ac7112054bd80a680c6b64ea80a41bf855b92a
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-01-21 10:15:15 +0530

    printing names from array

---
+++ demo25.java
@@ -0,0 +1,12 @@
+public class demo25 {
+    public static void main(String[] args) {
+        String arr[] = {"Yaswanth","Raghu","Raaju","Ramesh","Suresh"};
+
+        for(String Name:arr){
+            System.out.println(Name);
+
+
+        }
+        
+    }
+}
