532e7a14145e2a3bdb3d38591fee1239b6511d74
Madhav-0712 <yenamalamadhav5@gmail.com>
2025-02-16 22:01:10 +0530

    Demo25.java

--- demo25.java
+++ demo25.java
@@ -1,12 +1,20 @@
 public class demo25 {
     public static void main(String[] args) {
-        String arr[] = {"Yaswanth","Raghu","Raaju","Ramesh","Suresh"};
+       
+        String arr[][] = {
+            {"Yaswanth", "9876543210", "New York, USA"},
+            {"Raghu", "8765432109", "Los Angeles, USA"},
+            {"Raaju", "7654321098", "Chicago, USA"},
+            {"Ramesh", "6543210987", "Houston, USA"},
+            {"Suresh", "5432109876", "San Francisco, USA"}
+        };
 
-        for(String Name:arr){
-            System.out.println(Name);
-
-
-        }
         
+        for (String[] friend : arr) {
+            System.out.println("Name: " + friend[0]);
+            System.out.println("Phone: " + friend[1]);
+            System.out.println("Address: " + friend[2]);
+            System.out.println("----------------------");
+        }
     }
 }
